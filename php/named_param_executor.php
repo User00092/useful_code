fcd7951a74ec97ea6b61cc7c1fbf4a408aebfb58
User0092 <103274860+User00092@users.noreply.github.com>
2023-12-10 17:20:44 -0500

    Created named_param_executor.php
    This PHP file contains a function named executeWithNamedParameters designed for executing prepared SQL statements with named parameters. The function facilitates parameter binding and execution using the mysqli extension, allowing for enhanced readability and flexibility in database interactions.

---
+++ php/named_param_executor.php
@@ -0,0 +1,47 @@
+function executeWithNamedParameters(\mysqli $conn, string $sql, array $namedValues): false|\mysqli_stmt  {
+    $newSQL = "";
+    $listValues = [];
+    $listValuesTypes = '';
+
+    $parts = explode(' ', $sql);
+    foreach ($parts as $part) {
+        $part = trim($part);
+
+        if (str_starts_with($part, ":")) {
+            $newSQL .= " ?";
+
+            $value = $namedValues[$part] ?? null;
+
+            if ($value == null) {
+                throw new \Exception("Error retrieving the key: \"$part\"");
+            }
+
+            $listValues[] = $value;
+            if (is_int($value)) {
+                $listValuesTypes .= 'i';
+            } else if (is_double($value)) {
+                $listValuesTypes .= 'd';
+            } else if (is_string($value)) {
+                $listValuesTypes .= 's';
+            }
+
+        } else {
+            $newSQL .= " " . $part;
+        }
+    }
+
+    $stmt = $conn->prepare($newSQL);
+    if (!$stmt) {
+        throw new \Exception("Error in preparing statement: " . $conn->error);
+    }
+
+    $stmt->bind_param($listValuesTypes, ...$listValues);
+
+
+
+    if (!$stmt->execute()) {
+        return false;
+    }
+
+    return $stmt;
+}
