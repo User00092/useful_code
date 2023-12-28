0df17a62373bb549d9435fdaf5ddada2c3b021e0
User0092 <103274860+User00092@users.noreply.github.com>
2023-12-28 15:10:44 -0500

    Update time_based_point_calculation.py

--- Python/time_based_point_calculation.py
+++ Python/time_based_point_calculation.py
@@ -1,8 +1,12 @@
-def calculate_points(max_points: float, min_points: float, max_time: float, time_taken: float):
+def calculate_points(max_points: float, min_points: float, max_time: float, time_taken: float, reverse: bool = False):
     if time_taken <= 0:
-        return max_points
+        return max_points if not reverse else min_points
     elif time_taken >= max_time:
-        return min_points
+        return min_points if not reverse else max_points
     else:
-        slope = (max_points - min_points) / max_time
-        return max_points - slope * time_taken
+        if not reverse:
+            slope = (max_points - min_points) / max_time
+            return max_points - slope * time_taken
+        else:
+            slope = (max_points - min_points) / max_time
+            return min_points + slope * time_taken
